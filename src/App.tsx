20ff4f1d8a12378467d580ecb47138be1478ee96
GreenRoot <1.ooo.ooo@bk.ru>
2023-02-06 23:03:26 +0000

    added Themes - dark and light

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React, { Suspense } from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
-import './index.scss';
+import './styles/index.scss';
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+
 
 const App = () => {
+    const {theme, setTheme} = useContext(ThemeContext);
+
+    const toggleTheme = () => {
+        setTheme( theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+    }
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle Theme</button> <br/>
             <Link to={"/"}>Main page</Link>
             <Link to={"/about"}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
