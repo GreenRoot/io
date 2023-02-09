84e5e09b0bc2d7d591b23bbc467c33a84c6c3a80
GreenRoot <1.ooo.ooo@bk.ru>
2023-02-09 17:30:57 +0300

    Defined Routes at the config level

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from "react-router-dom";
+import { Link} from "react-router-dom";
 import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "app/providers/router";
 
 
 const App = () => {
@@ -14,12 +12,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle Theme</button> <br/>
             <Link to={"/"}>Main page</Link>
             <Link to={"/about"}>About</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={"/about"} element={<AboutPage />} />
-                    <Route path={"/"} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
