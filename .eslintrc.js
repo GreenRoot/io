aa40a6df4a34197bfceb717f500c071ed5486aa7
GreenRoot <1.ooo.ooo@bk.ru>
2023-02-22 11:57:25 +0300

    Fix tsconfig.json and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': [
             'error',
-            { markupOnly: true, ignoreAttribute: ['data-testid'] },
+            { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
@@ -52,7 +52,7 @@ module.exports = {
     },
     overrides: [
         {
-            files: ['**/src/**/*.test.{ts, tsx}'],
+            files: ['**/src/**/*.test.tsx', '**/src/**/*.test.ts'],
             rules: {
                 'i18next/no-literal-string': 'off',
             },
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import {
     RenderWithTranslation,
 } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
