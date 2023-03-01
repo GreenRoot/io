797a6c5b84a4936b211b17f2db61628c61e20055
GreenRoot <1.ooo.ooo@bk.ru>
2023-03-01 23:05:37 +0300

    Links icons added

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -6,6 +6,8 @@ import Button, { ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import MainIcon from 'shared/assets/icons/main.svg';
+import AboutIcon from 'shared/assets/icons/about.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -27,17 +29,24 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
                     to={RoutePath.main}
+                    className={cls.item}
                 >
-                    {t('Главная страница')}
+                    <MainIcon className={cls.icon} />
+                    <span className={cls.link}>
+                        {t('Главная страница')}
+                    </span>
                 </AppLink>
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
                     to={RoutePath.about}
+                    className={cls.item}
                 >
-                    {t('О нас')}
+                    <AboutIcon className={cls.icon} />
+                    <span className={cls.link}>
+                        {t('О нас')}
+                    </span>
                 </AppLink>
             </div>
-
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher short={collapsed} className={cls.lang} />
