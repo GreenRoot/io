769290dd92dc0828bb15c57d29a2453fbb27cf46
GreenRoot <1.ooo.ooo@bk.ru>
2023-03-14 11:00:22 +0300

    Portal and Modal added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,24 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { t } from 'i18next';
 
 const App = () => {
     const { theme } = useTheme();
+
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <button type="button" onClick={() => setIsOpen(true)}>Toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    {t('Lorem')}
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
